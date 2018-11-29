dedb481a8d6e78bef5c2beedb96e4e4aab4fe063
Paige Watson <paige.watson@outlook.com>
2018-11-29 15:04:27 -0800

    Add format_list_as_string

--- lib/power_set_elixir.ex
+++ lib/power_set_elixir.ex
@@ -2,4 +2,12 @@ defmodule ElixirPowerSet do
   def split_string_to_list(textToParse) do
     String.split(textToParse, ",", trim: true)
   end
+
+  def format_list_as_string(list_to_format) do
+    Enum.map(
+      list_to_format,
+      fn list -> "{#{list}}" end
+    )
+    |> Enum.join(",")
+  end
 end
--- test/power_set_elixir_test.exs
+++ test/power_set_elixir_test.exs
@@ -5,5 +5,10 @@ defmodule ElixirPowerSetTest do
     test "Should split string to list" do
       assert ["a", "b"] == ElixirPowerSet.split_string_to_list("a,b")
     end
+
+    test "Should Return output as formatted string" do
+      expectedOutput = "{},{a}"
+      assert expectedOutput == ElixirPowerSet.format_list_as_string([[], ["a"]])
+    end
   end
 end
